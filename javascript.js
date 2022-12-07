0ac5c0d97c76d0427af13566482e44bff4d63b14
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-07 20:30:35 +0100

    Add Generate New Grid button to the site

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const content = document.querySelector(".content");
+const generate = document.querySelector(".generate-button");
 
 for (let rows = 0; rows < 16; rows++) {
   for (let cols = 0; cols < 16; cols++) {
