1ddcb4c7f113fb1993bedc892c15db39038c457c
Szymon Fratczak <42326374+sfratczak@users.noreply.github.com>
2022-12-07 20:54:47 +0100

    Add option to generate grid of different sizes

--- javascript.js
+++ javascript.js
@@ -1,16 +1,31 @@
 const content = document.querySelector(".content");
 const generate = document.querySelector(".generate-button");
 
-for (let rows = 0; rows < 16; rows++) {
-  for (let cols = 0; cols < 16; cols++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
+const CONTENT_SIZE_IN_PX = 500;
+content.style.height = `${CONTENT_SIZE_IN_PX}px`;
+content.style.width = `${CONTENT_SIZE_IN_PX}px`;
 
-    box.addEventListener(
-      "mouseover",
-      () => (box.style.backgroundColor = "black")
-    );
+generateNewGrid(16);
 
-    content.appendChild(box);
+function generateNewGrid(size) {
+  if (typeof size != "number" || size < 1 || size > 100)
+    return "Please provide a number in range 1-100";
+
+  let boxSize = CONTENT_SIZE_IN_PX / size;
+
+  for (let rows = 0; rows < size; rows++) {
+    for (let cols = 0; cols < size; cols++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.height = `${boxSize}px`;
+      box.style.width = `${boxSize}px`;
+
+      box.addEventListener(
+        "mouseover",
+        () => (box.style.backgroundColor = "black")
+      );
+
+      content.appendChild(box);
+    }
   }
 }
